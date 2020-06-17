e82fa01a84ca09e928253f620b9f916c69b1d1d3
Salaiaung40 <60382946+Salaiaung40@users.noreply.github.com>
2020-06-17 11:19:29 +0200

    Chage 8
    change 8

--- src/main.js
+++ src/main.js
@@ -1,24 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 import firebase from "firebase/app";
+import VueNativeSock from "vue-native-websocket";
 // import $ from 'jquery'
 // import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 // import 'bootstrap/dist/css/bootstrap.css';
 // import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueNativeSock, "ws://localhost:9090");
 // Vue.use(BootstrapVue);
-// var $ = require('jquery') 
-var Scrollactive = require('vue-scrollactive');
+// var $ = require('jquery')
+var Scrollactive = require("vue-scrollactive");
 Vue.use(Scrollactive);
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
-
-
-
+  render: (h) => h(App),
+}).$mount("#app");
 
 var firebaseConfig = {
   apiKey: "AIzaSyC-Bz3n31G7mgpzd1ipHxlrLsbuo2wywLE",
@@ -28,8 +27,8 @@ var firebaseConfig = {
   storageBucket: "contact-form-cc17e.appspot.com",
   messagingSenderId: "849564447345",
   appId: "1:849564447345:web:5e7a0be35674a227cde1d0",
-  measurementId: "G-S7QX2H45LP"
+  measurementId: "G-S7QX2H45LP",
 };
-  // Initialize Firebase
+// Initialize Firebase
 
 firebase.initializeApp(firebaseConfig);
